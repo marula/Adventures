c755b53f5cc071887a754c7b84407d88d1a8aaaf
Rick Kleinhans <rick@baobablogic.com>
2012-04-14 02:30:32 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goa::Application.config.secret_token = 'de890ec4b19ab5224939533185c6e98843032933289d402a0c3b08ec00fff156c824d5efcc28950856604705944e532cf4c7526b48a37c728803bee6a8a6fb6a'
